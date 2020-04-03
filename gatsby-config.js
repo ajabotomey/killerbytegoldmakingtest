afaaaf4609e6c8217c0999e51650ee3869d063ad
AJ Abotomey <killerbyte_93@hotmail.com>
2020-04-03 15:03:20 +1000

    Take 2 deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,13 +2,13 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Starter Blog`,
     author: {
-      name: `Kyle Mathews`,
+      name: `AJ Abotomey`,
       summary: `who lives and works in San Francisco building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `ajabotomey`,
     },
   },
   plugins: [
